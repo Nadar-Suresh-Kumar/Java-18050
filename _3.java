7d1cc600060e65a1c19b909fbfad2b6a7f4bbaae
Nadar-Suresh-Kumar <146653769+Nadar-Suresh-Kumar@users.noreply.github.com>
2024-02-24 08:08:34 +0530

    Add files via upload

---
+++ _3.java
@@ -0,0 +1,9 @@
+class Main{
+  public static void main(String args[]){
+      int num;
+      num=200;
+      System.out.println(" Value of num is :"+num);
+      num=200*2;
+      System.out.println(" Value of num*2 is :"+num);
+    }
+  }
